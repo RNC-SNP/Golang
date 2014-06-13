c3b30ca3249023384950eee9e07a982e091dacff
Rinc <i@RincLiu.com>
2014-06-13 15:57:55 +0800

    Sort, Iter.

--- data/database/mongodb/mongodb.go
+++ data/database/mongodb/mongodb.go
@@ -6,9 +6,9 @@ import (
 	"labix.org/v2/mgo/bson"
 )
 
-type Platform struct {
+type User struct {
 	Name string
-	Company string
+	Age int
 }
 
 func main() {
@@ -19,17 +19,31 @@ func main() {
 	//session.SetMode(mgo.Monotonic, true)
 	
 	// Select DB and collection:
-	collection := session.DB("test").C("platform")
+	collection := session.DB("test").C("users")
 	
 	// Insert values:
-	err = collection.Insert(&Platform{"Android", "Google"}, &Platform{"iOS", "Apple"})
+	err = collection.Insert(&User{"Adam", 14}, &User{"Bob", 19}, &User{"Chris", 10}, &User{"Dennis", 35}, &User{"Emma", 26}, &User{"Frank", 41})
 	checkErr(err)
+
+	u := User{}
 	
-	// Query:
-	result := Platform{}
-	err = collection.Find(bson.M{"name": "Android"}).One(&result)
+	// Query first result:
+	err = collection.Find(bson.M{"name": "Emma"}).One(&u)
 	checkErr(err)
-	fmt.Println("Company:", result.Company)
+	fmt.Println("Emma's age:", u.Age)
+
+	// Query all and sort:
+	query1 := collection.Find(bson.M{"age": bson.M{"$gte": 10, "$lte": 50}}).Sort("name", "-age")
+
+	// Travel result set:
+	iter := query1.Iter()
+	for iter.Next(&u) {
+    		fmt.Printf("%s: %d\n", u.Name, u.Age)
+	}
+	// Close iter:
+	if err := iter.Close(); err != nil {
+    		panic(err)
+	}
 }
 
 func checkErr(err error) {

17971a8965bde7758ba7d2297322d99182a62fbd
Rinc <i@RincLiu.com>
2014-06-13 16:24:39 +0800

    RemoveAll.

--- data/database/mongodb/mongodb.go
+++ data/database/mongodb/mongodb.go
@@ -20,18 +20,23 @@ func main() {
 	
 	// Select DB and collection:
 	collection := session.DB("test").C("users")
+
+	// Delete values:
+	// DELETE FROM users WHERE age >= 0;
+	change, err := collection.RemoveAll(bson.M{"age": bson.M{"$gte": 0}})
+	checkErr(err)
+	fmt.Printf("updated %d and removed %d after executing RemoveAll().\n", change.Updated, change.Removed)
 	
 	// Insert values:
 	err = collection.Insert(&User{"Adam", 14}, &User{"Bob", 19}, &User{"Chris", 10}, &User{"Dennis", 35}, &User{"Emma", 26}, &User{"Frank", 41})
 	checkErr(err)
-
-	u := User{}// Temp var to store query result
 	
 	// Query and sort:
 	// SELECT * FROM users WHERE age >= 10 && age <= 50 ORDER BY name ASC, age DESC;
 	query := collection.Find(bson.M{"age": bson.M{"$gte": 10, "$lte": 50}}).Sort("name", "-age")
 	// Travel result set:
 	iter := query.Iter()
+	u := User{}
 	for iter.Next(&u) {
     		fmt.Printf("%s: %d\n", u.Name, u.Age)
 	}

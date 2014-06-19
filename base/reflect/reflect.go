f83815a1dc269a0b5aea82a8204433f85e4530b6
Rinc <i@RincLiu.com>
2014-06-19 19:17:30 +0800

    Use Elem to modifiy value.

--- base/reflect/reflect.go
+++ base/reflect/reflect.go
@@ -5,16 +5,27 @@ import (
 	"reflect"
 )
 
-type Dish struct {
-	Id int
+type Person struct {
 	Name string
-	Query func()
+	Age int
 }
 
 func main() {
-	for name, mtype := range attributes(&Dish{}) {
-		fmt.Printf("Name: %s, Type: %s\n", name, mtype.Name())
-	}		
+	person := Person{"Rinc", 25}
+	
+	v1 := reflect.ValueOf(person.Name)
+	fmt.Printf("value of name: %v\n", v1)
+	fmt.Printf("settability of name: %v\n", v1.CanSet())
+	
+	// Use pointer and Elem to modify value:
+	v2 := reflect.ValueOf(&person.Name).Elem()
+	fmt.Printf("settability of name: %v\n", v2.CanSet())
+	v2.SetString("Rinc Liu")
+	fmt.Printf("value of name: %v\n", person.Name)
+
+	for aName, aType := range attributes(&person) {
+		fmt.Printf("attrName: %s, attrType: %s\n", aName, aType.Name())
+	}
 }
 
 func attributes(x interface{}) (map[string]reflect.Type) {
@@ -24,11 +35,13 @@ func attributes(x interface{}) (map[string]reflect.Type) {
 	}
 	
 	attrs := make(map[string]reflect.Type)
+
 	if typ.Kind() != reflect.Struct {
 		fmt.Printf("%v type can't have attributes inspected\n", typ.Kind())
 		return attrs
 	}
 	
+	// Travel all fields:
 	for i := 0; i < typ.NumField(); i++ {
 		if f := typ.Field(i); !f.Anonymous {
 			attrs[f.Name] = f.Type
